5654f3e1a7b55096ee3d3df602d7c6c5cef535df
Sean Bowe <ewillbefull@gmail.com>
2016-03-17 13:12:55 -0600

    Tidy up snark/gadget code

--- src/snark.hpp
+++ src/snark.hpp
@@ -5,9 +5,24 @@
 #include <boost/optional.hpp>
 
 using namespace libsnark;
+using namespace std;
+
+#include "gadget.hpp"
 
 template<typename ppzksnark_ppT>
-r1cs_ppzksnark_keypair<ppzksnark_ppT> generate_keypair();
+r1cs_ppzksnark_keypair<ppzksnark_ppT> generate_keypair()
+{
+    typedef Fr<ppzksnark_ppT> FieldT;
+
+    protoboard<FieldT> pb;
+    l_gadget<FieldT> g(pb);
+    g.generate_r1cs_constraints();
+    const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
+
+    cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
+
+    return r1cs_ppzksnark_generator<ppzksnark_ppT>(constraint_system);
+}
 
 template<typename ppzksnark_ppT>
 boost::optional<r1cs_ppzksnark_proof<ppzksnark_ppT>> generate_proof(r1cs_ppzksnark_proving_key<ppzksnark_ppT> proving_key,
@@ -16,7 +31,21 @@ boost::optional<r1cs_ppzksnark_proof<ppzksnark_ppT>> generate_proof(r1cs_ppzksna
                                                                    const bit_vector &x,
                                                                    const bit_vector &r1,
                                                                    const bit_vector &r2
-                                                                   );
+                                                                   )
+{
+    typedef Fr<ppzksnark_ppT> FieldT;
+
+    protoboard<FieldT> pb;
+    l_gadget<FieldT> g(pb);
+    g.generate_r1cs_constraints();
+    g.generate_r1cs_witness(h1, h2, x, r1, r2);
+
+    if (!pb.is_satisfied()) {
+        return boost::none;
+    }
+
+    return r1cs_ppzksnark_prover<ppzksnark_ppT>(proving_key, pb.primary_input(), pb.auxiliary_input());
+}
 
 template<typename ppzksnark_ppT>
 bool verify_proof(r1cs_ppzksnark_verification_key<ppzksnark_ppT> verification_key,
@@ -24,7 +53,11 @@ bool verify_proof(r1cs_ppzksnark_verification_key<ppzksnark_ppT> verification_ke
                   const bit_vector &h1,
                   const bit_vector &h2,
                   const bit_vector &x
-                  // ...
-                 );
+                 )
+{
+    typedef Fr<ppzksnark_ppT> FieldT;
 
-#include "snark.tcc"
+    const r1cs_primary_input<FieldT> input = l_input_map<FieldT>(h1, h2, x);
+
+    return r1cs_ppzksnark_verifier_strong_IC<ppzksnark_ppT>(verification_key, input, proof);
+}
